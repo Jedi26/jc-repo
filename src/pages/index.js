a88dd3c153db44b262f8833aeeb1673a6f09b673
Jedi <34927106+Jedi26@users.noreply.github.com>
2023-06-19 05:36:22 -0700

    Update index.js
    added links

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 import { Link } from 'gatsby'
-//import Repo from '../components/static_queries/Repo'
-//import Repo2 from './repo'
+
 
 
 
@@ -54,7 +53,7 @@ class HomeIndex extends React.Component {
                             <br/> 
                             <br/>
                             <h3>How to Get GitHub Access Tokens</h3>
-                            <p> Don't forget to read the whole <a href="/">tutorial</a> here to set your environment variables for your GitHub Access Tokens.</p>
+                            <p> Don't forget to read the whole <a href="https://medium.com/@jedalvarezuybentillo/jc-repo-guide-c5b1d9a10a04">tutorial</a> here to set your environment variables for your GitHub Access Tokens.</p>
                             <pre>
                                     <code>
                                        GITHUB_PERSONAL_ACCESS_TOKEN=<br/>
@@ -64,7 +63,7 @@ class HomeIndex extends React.Component {
                              <ul className="actions">
                                     
                                     <li> <Link to="/repo" className="button"> VIEW REPOSITORIES </Link> </li>
-                                    <li> <Link to="/" className="button"> READ TUTORIAL </Link> </li>
+                                    <li> <Link to="https://medium.com/@jedalvarezuybentillo/jc-repo-guide-c5b1d9a10a04" className="button"> READ TUTORIAL </Link> </li>
                              
                                 </ul>
                         </div>
